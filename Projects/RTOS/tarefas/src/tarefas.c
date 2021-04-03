dd3a4d65f8ad02fdf62fa7b963a8b6cc2f480889
bruninxd <79425278+bruninxd@users.noreply.github.com>
2021-04-03 02:52:15 -0300

    Lab 4-ex2 finalizado

--- Projects/RTOS/tarefas/src/tarefas.c
+++ Projects/RTOS/tarefas/src/tarefas.c
@@ -1,120 +1,54 @@
+//ALUNO BRUNO SOARES DE SOUZA
+//1558781
+
 #include "system_tm4c1294.h" // CMSIS-Core
 #include "driverleds.h" // device drivers
 #include "cmsis_os2.h" // CMSIS-RTOS
 
-osThreadId_t thread1_id, thread2_id;
+osThreadId_t thread1_id, thread2_id, thread3_id, thread4_id;
+
+typedef struct{
+  uint8_t num_LED;
+  uint32_t time_LED;
+}LED_struct;
 
 void MyThread(void *arg)
 {
-  
   uint8_t state = 0;
   uint32_t tick;
   
-  while(1){
+  while(1)
+  {
     tick = osKernelGetTickCount();
+
+    state ^= ((LED_struct*)arg)->num_LED;
+    LEDWrite(((LED_struct*)arg)->num_LED, state);
     
-    state ^= (uint8_t)arg;
-    LEDWrite((uint8_t)arg, state);
-    
-    osDelayUntil(tick + 100);
+    osDelayUntil(tick + ((LED_struct*)arg)->time_LED);
   } // while
 }  
-//  
-//  uint8_t *Ponteiro;
-//  
-//  
-//  
-//  Ponteiro = arg;
-//  
-// // int param;
-//  int ala = (int)arg;
-//  param = (int) arg;
-//
-//  
-//  uint8_t lala;
-//  
-//  lala = 1;
-//  lala = 2;
-//  
-  
-  //lala = (int) arg;
-  
-//  
-//  if ((int)arg == 1 )
-//  {
-//  while(1){
-//    state ^= LED1;
-//    LEDWrite(LED1, state);
-//    
-//    osDelay(100);
-//  } // while
-//  }
-//} // thread1
-//
-//void thread2(void *arg){
-//  uint8_t state = 0;
-//  uint32_t tick;
-//  
-//  while(1){
-//    tick = osKernelGetTickCount();
-//    
-//    state ^= LED2;
-//    LEDWrite(LED2, state);
-//    
-//    osDelayUntil(tick + 100);
-//  } // while
-//} // thread2
-
-//void MyThread(void *arg)
-//{
-//  uint8_t state = 0;
-//  
-//  int valor, *Pvalor;
-//
-//  Pvalor = arg;
-////  valor = *Pvalor;
-//  
-//  valor = (int) arg;
-//  
-//  //if arg == 1 -> led 1
-//  //if arg == 2 -> led 2
-//  if (*Pvalor == 1)
-//  {
-//      while(1)
-//      {
-//      state ^= LED1;
-//      LEDWrite(LED1, state);
-//      osDelay(100);
-//      } // while
-//    // thread1
-//  }
-//
-//  uint32_t tick;
-//  
-//  while(1)
-//  {
-//    tick = osKernelGetTickCount();
-//    
-//    state ^= LED2;
-//    LEDWrite(LED2, state);
-//    
-//    osDelayUntil(tick + 100);
-//  } // while
-//} // thread2
-
+    
 void main(void){
-  LEDInit(LED2 | LED1);
-
-//  uint8_t entrada1, entrada2;
+  LEDInit(LED4 |LED3 |LED2 | LED1);
   
-//  entrada1 = 1;
-//  entrada2 = 2;
-//  
+  
+  LED_struct LED_stru1, LED_stru2, LED_stru3, LED_stru4;
+  
+  LED_stru1.num_LED = 1;
+  LED_stru1.time_LED = 200;
+  LED_stru2.num_LED = 2;
+  LED_stru2.time_LED = 300;
+  LED_stru3.num_LED = 4;
+  LED_stru3.time_LED = 500;
+  LED_stru4.num_LED = 8;
+  LED_stru4.time_LED = 700;
+  
   osKernelInitialize();
 
-  
-  thread1_id = osThreadNew(MyThread, (void*)1, NULL);// (void*)entrada1, NULL);
-  thread2_id = osThreadNew(MyThread, (void*)2, NULL);
+  thread1_id = osThreadNew(MyThread, (void*)&LED_stru1, NULL);
+  thread2_id = osThreadNew(MyThread, (void*)&LED_stru2, NULL);
+  thread3_id = osThreadNew(MyThread, (void*)&LED_stru3, NULL);
+  thread4_id = osThreadNew(MyThread, (void*)&LED_stru4, NULL);
 
   if(osKernelGetState() == osKernelReady)
     osKernelStart();
